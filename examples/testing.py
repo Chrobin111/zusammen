3b73784f67d66a057083e51fb58a2a6f0ec23ecb
Christopher <chhecker@mpe.mpg.de>
2022-08-17 20:25:17 +0200

    Created a analysis script to run from terminal

---
+++ examples/testing.py
@@ -0,0 +1,54 @@
+import warnings
+
+warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore")
+
+from cosmogrb.universe.survey import Survey
+
+import os, sys
+
+parent_dir = os.path.abspath("..")
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from zusammen.stan_models.stan_model import get_model
+from zusammen import AnalysisBuilder, DataSet
+from zusammen.spectral_plot import display_posterior_model_counts
+
+import arviz as av
+
+update_logging_level("FATAL")
+
+
+survey = Survey.from_file("data/survey.h5")
+ab = AnalysisBuilder(survey, use_bb=True)
+ab.write_yaml("test_proc.yml")
+ds = DataSet.from_yaml("test_proc.yml")
+
+
+m = get_model("cpl_simple_chunked_gc")
+m.clean_model()
+m.build_model()
+
+
+data = ds.to_stan_dict()
+
+n_threads = 3
+n_chains = 2
+
+fit = m.model.sample(
+    data=data,
+    parallel_chains=n_chains,
+    chains=n_chains,
+    # inits= {'alpha':-1.},
+    threads_per_chain=n_threads,
+    seed=1234,
+    iter_warmup=1000,
+    iter_sampling=500,
+    max_treedepth=12,
+    show_progress=True,
+)
+
+
+res = av.from_cmdstanpy(fit)
+res.to_netcdf("inference_data/test_inference_data_energy_flux_5.nc")
