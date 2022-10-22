6fce2776a60d705cf97fc8f86a79aa242a0874ce
Christopher <chhecker@mpe.mpg.de>
2022-10-22 14:40:13 +0200

    Analytic runs

--- examples/run_combined.py
+++ examples/run_combined.py
@@ -1,13 +1,4 @@
-from pathlib import Path
-
-import numba as nb
 import numpy as np
-import scipy.stats as stats
-
-from natsort import natsorted
-
-import matplotlib.pyplot as plt
-
 
 # plt.style.use("mike")
 import warnings
@@ -15,23 +6,11 @@ import warnings
 warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore")
 
-
-import astropy.units as u
-
-import cmasher as cmr
-
-green = "#33FF86"
-purple = "#CE33FF"
-
-
-from cosmogrb.universe.survey import Survey
-
 import os, sys
 
 parent_dir = os.path.abspath("..")
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
-
 from zusammen.stan_models.stan_model import get_model
 from zusammen import AnalysisBuilder, DataSet
 from zusammen.spectral_plot import display_posterior_model_counts
@@ -44,30 +23,20 @@ import arviz as av
 update_logging_level("FATAL")
 
 
-from astromodels import Band_Calderone, PointSource, Model
-
-
-from threeML import JointLikelihood, DataList, display_spectrum_model_counts
-
-from astromodels import Cutoff_powerlaw
-
-import popsynth as ps
-
-ds = DataSet.from_yaml("test_proc.yml")
-
-import cmdstanpy
+# ds = DataSet.from_yaml("test_proc_sig_10_red.yml")
+ds = DataSet.from_hdf5_file("sgrb.h5")
 
 # cmdstanpy.set_cmdstan_path("/home/bjorn/general_sw/cmdstan-2.29.0/")
 
 m = get_model("cpl_simple_chunked_combined")
 
-m.clean_model()
+# m.clean_model()
 
 m.build_model()  # opt_exp=True)
 
 data = ds.to_stan_dict()
 
-n_threads = 4
+n_threads = 2
 n_chains = 2
 
 fit = m.model.sample(
@@ -89,10 +58,10 @@ fit = m.model.sample(
     iter_sampling=500,
     max_treedepth=15,
     adapt_delta=0.9,
-    show_progress=True,
-    # show_console=True
-    output_dir=".stan/",
+    # show_progress=True,
+    show_console=True
+    # output_dir=".stan/",
 )
 
 res = av.from_cmdstanpy(fit)
-res.to_netcdf("res/combined_all.nc")
+res.to_netcdf("inference_data/analytic.nc")
