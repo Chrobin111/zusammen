e9a1c3a09961bdfc829b488e5f6fc93e44c7062a
Christopher <chhecker@mpe.mpg.de>
2022-11-25 10:55:00 +0100

    Changed inferences

--- analysis/inference.py
+++ analysis/inference.py
@@ -17,7 +17,7 @@ inference_folder = "inference/"
 data_folder = "real_data/"
 data_name = "data"
 
-model_name = "cpl_simple_chunked_gc_relaxed"
+model_name = "cpl_simple_chunked_gc"
 inference_name = "real_sig_5_1000"
 
 ds = DataSet.from_hdf5_file(data_folder + data_name + ".h5")
